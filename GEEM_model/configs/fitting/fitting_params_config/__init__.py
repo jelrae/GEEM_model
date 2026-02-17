4acf7b69752cbd3120c6481dbd194ee8f80ed474
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2026-02-17 10:52:55 +0100

    Updated the __init__.pyto have the new bounds

--- GEEM_model/configs/fitting/fitting_params_config/__init__.py
+++ GEEM_model/configs/fitting/fitting_params_config/__init__.py
@@ -1,3 +1,3 @@
-from . import full_model_full_fit_wide_bounds, full_model_full_fit_AI_bounds
+from . import full_model_full_fit_wide_bounds, full_model_full_fit_AI_bounds, full_model_full_fit_lit_bounds
 
-__all__ = ['full_model_full_fit_wide_bounds', 'full_model_full_fit_AI_bounds']
+__all__ = ['full_model_full_fit_wide_bounds', 'full_model_full_fit_AI_bounds', 'full_model_full_fit_lit_bounds']
