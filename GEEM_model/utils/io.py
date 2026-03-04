36af4f11c0735347c71d69650f9f7a625eab4b72
Jordan Earle <43170680+jelrae@users.noreply.github.com>
2026-03-04 14:52:38 +0100

    fixed the save params function

--- GEEM_model/utils/io.py
+++ GEEM_model/utils/io.py
@@ -16,7 +16,6 @@ def create_dirs(fps):
     :param fps: a list of strings which contain the filepaths desired to create
     :return:
     """
-
     for p in fps:
         os.makedirs(p, exist_ok=True)
         print("The directory " + p + " is created!")
@@ -28,8 +27,8 @@ def load_params(fp):
     return ep
 
 
-def save_params(fp, p):
+def save_params(fp, fn, p):
     print("Current working directory:", os.getcwd())
     create_dirs([fp])
-    with open(fp, "wb") as pickie:
+    with open(fp + fn, "wb") as pickie:
         pickle.dump(p, pickie)
